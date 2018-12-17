0f4ecc9c532216ffc8268f3822cd16ff07def0cd
Marcus Plenty <marcusplenty@gmail.com>
2018-12-17 04:58:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -62,7 +62,7 @@ class Person
   def call_friend(friend)
     friend.happiness += 3
     self.happiness += 3
-    return "Hi #{friend.name}! It's #{person.name}. How are you?"
+    return "Hi #{friend.name}! It's #{self.name}. How are you?"
   end 
   
 
