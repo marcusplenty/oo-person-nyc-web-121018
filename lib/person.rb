795026d15869fa57f29e4293a7c758986a36b00d
Marcus Plenty <marcusplenty@gmail.com>
2018-12-17 04:43:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,7 +9,7 @@ class Person
     @hygiene = 8
   end
   def happiness=(amount)
-    @happiness += amount
+    @happiness = @happinness+amount
   end 
   # def hygiene=(amount)
   #   @hygiene+= amount
